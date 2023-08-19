c404e3e4bc9c3471a910769a8629524383b4ddda
MohamedBidouk <92817844+MohamedBidouk@users.noreply.github.com>
2023-08-19 14:45:35 +0100

    first commit

--- src/main/java/com/carbox/apimanager/ApimanagerApplication.java
+++ src/main/java/com/carbox/apimanager/ApimanagerApplication.java
@@ -2,12 +2,21 @@ package com.carbox.apimanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApimanagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApimanagerApplication.class, args);
+    }
+    @GetMapping("/Apimanager/status")
+    public ResponseEntity<?> getApiManagerStatus(){
+        return new ResponseEntity<>("I'm active api manager", HttpStatus.OK);
     }
 
 }
